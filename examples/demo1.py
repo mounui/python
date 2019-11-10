5feea6b014ba90b4e63a534bc4d8abb269328c34
mao <maojunhui5214@163.com>
2019-11-10 18:04:58 +0800

    add files

---
+++ examples/demo1.py
@@ -0,0 +1,6 @@
+print('hello world!')
+
+name = input('please enter your name:')
+print('hello,',name)
+
+print(1024 * 768)
---
+++ examples/demo2.py
@@ -0,0 +1,8 @@
+print('''line1
+line2
+line3''')
+
+print(r'''hello,\n
+world''')
+
+print(r'hello \'\n world')
