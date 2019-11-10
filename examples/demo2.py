e932d528ebc6d1b1d7dcc0fc9feda5695501d6fa
mao <maojunhui5214@163.com>
2019-11-10 21:49:13 +0800

    update demo2.py

--- examples/demo2.py
+++ examples/demo2.py
@@ -6,3 +6,32 @@ print(r'''hello,\n
 world''')
 
 print(r'hello \'\n world')
+
+a = 123  # a是整数
+print(a)
+a = 'ABC'  # a变为字符串
+print(a)
+
+a = 'ABC'
+b = a
+a = 'XYZ'
+print(b)
+
+n = 123
+f = 456.789
+s1 = 'Hello,world'
+s2 = 'Hello,\'Adam\''
+s3 = r'Hello,"Bart"'
+s4 = r'''Hello,
+Lisa!'''
+print('n =',n)
+print('f =',f)
+print('s1 =',s1)
+print('s2 =',s2)
+print('s3 =',s3)
+print('s4 =',s4)
+
+print(ord('A'))  # 获取字符的整数表示
+print(ord('中'))
+print(chr(66))  # 把编码转换为对应的字符
+print(chr(25991))
