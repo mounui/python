b3f35c62069986d4b0c4dc3bac91e1a36d769151
epins <2401052316@qq.com>
2019-11-11 16:27:32 +0800

    update demo2.py

--- examples/demo2.py
+++ examples/demo2.py
@@ -35,3 +35,9 @@ print(ord('A'))  # 获取字符的整数表示
 print(ord('中'))
 print(chr(66))  # 把编码转换为对应的字符
 print(chr(25991))
+
+s1 = 72
+s2 = 85
+res = (s2 - s1) / s1 * 100
+print(res)
+print('成绩提升百分比：%.1f' % res)
