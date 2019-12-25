bdd575203b92c74d4598da75bf20dff1fca06fcd
epins <2401052316@qq.com>
2019-12-25 17:54:07 +0800

    update psutil

---
+++ examples/do_psutil.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import psutil
+print('cpu逻辑数量：',psutil.cpu_count())
+print('CPU物理核心：',psutil.cpu_count(logical=False))
+print('统计CPU的用户/系统/空闲时间')
+print(psutil.cpu_times())
+
