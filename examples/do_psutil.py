2582006add4e65def6da1e04f09ebd4c0348387f
mao <maojunhui5214@163.com>
2019-12-30 23:00:29 +0800

    update do_psutil

--- examples/do_psutil.py
+++ examples/do_psutil.py
@@ -2,6 +2,5 @@
 import psutil
 print('cpu逻辑数量：',psutil.cpu_count())
 print('CPU物理核心：',psutil.cpu_count(logical=False))
-print('统计CPU的用户/系统/空闲时间')
-print(psutil.cpu_times())
+print('统计CPU的用户/系统/空闲时间：',psutil.cpu_times())
 
