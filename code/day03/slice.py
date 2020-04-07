1b7a75329aaaf94db066538581fa324c4aeec0c3
epins <2401052316@qq.com>
2020-04-07 18:27:31 +0800

    add day03

---
+++ code/day03/slice.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------
+# 利用切片操作去除字符串首尾空格
+# ----------------------------------
+
+def trim(s):
+    if len(s) == 0:
+        return s
+    elif s[0] == ' ':
+        return trim(s[1:])
+    elif s[-1] == ' ':
+        return trim(s[:-1])
+    return s
+
+# str = input('请输入字符串：')
+str = ' hello world   '
+print(str)
+print(trim(str))
+
+# ----------------------------------
+# 使用迭代查找一个list中的最小和最大值并返回一个tuple
+# ----------------------------------
+
+def findMinAndMax(L):
+    if L != []:
+        (max,min) = (L[0],L[0])
+        for v in L:
+            if v > max:
+                max = v
+            if v < min:
+                min = v
+        return(min,max)
+    else:
+        return(None, None)
+
+L = [1,3,4,5,2,8]
+print(findMinAndMax(L))
+
+# ----------------------------------
+# 斐波拉契数列
+# ----------------------------------
+
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield b
+        a, b = b, a + b
+        n = n + 1
+    return 'done'
+
+# ----------------------------------
+# 杨辉三角
+# ----------------------------------
+
+def triangles():
+    p = [1]
+    while True:
+        yield p
+        p = [1] + [p[i] + p[i+1] for i in range(len(p)-1)] + [1]
+
+n = 0
+for t in triangles():
+    print(t)
+    n = n + 1
+    if n == 10:
+        break
+
+# ----------------------------------
+# 利用map()函数，把用户输入的不规范的英文名字，变为首字母大写，其他小写的规范名字。
+# ----------------------------------
+
+def normalize(name):
+    return name[0].upper() + name[1:].lower()
+
+L1 = ['adam', 'LISA', 'barT']
+L2 = list(map(normalize, L1))
+print(L2)
+
+# ----------------------------------
+# 接受一个list并求积
+# ----------------------------------
+from functools import reduce
+
+def prod(L):
+    return reduce(lambda x, y: x * y, L)
+
+print(prod([1,3,5,9]))
