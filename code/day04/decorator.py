e40dbfe3dd324a50879727e53e8df23bb6e0b32b
mao <maojunhui5214@163.com>
2020-04-08 21:54:02 +0800

    update decorator

--- code/day04/decorator.py
+++ code/day04/decorator.py
@@ -5,8 +5,8 @@
 import time, functools
 
 def metric(fn):
-    @functools.wraps(fn)
     start = time.time()
+    @functools.wraps(fn)
     def wrapper(*args, **kw):
         r = fn(*args, **kw)
         print('call %s() cost %5f ms:' % (fn.__name__,1000*(time.time() - start)))
@@ -14,12 +14,41 @@ def metric(fn):
     return wrapper
 
 # 测试
-@metric()
+@metric
 def fast(x, y):
     time.sleep(0.0012)
     return x + y
 
-@metric()
+@metric
 def slow(x, y, z):
     time.sleep(0.1234)
     return x * y * z
+
+fast(1,3)
+slow(2,3,4)
+
+# ----------------------------------
+# 编写一个decorator在函数前后打印日志
+# ----------------------------------
+
+def log(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kw):
+        print('begin call: %s' % func.__name__)
+        c = func(*args, **kw)
+        print('end call: %s' % func.__name__)
+        return c
+    return wrapper
+
+@log
+def now():
+    print('在函数前后打印日志')
+
+now()
+
+# ----------------------------------
+# 偏函数使用
+# ----------------------------------
+
+int2 = functools.partial(int,base=2)
+print('int2(\'100000\'):',int2('100000'))
